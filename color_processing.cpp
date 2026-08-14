2b8508a56cb1feab969b7f345331b72f7a5f86e7
songsan <22000375@handong.ac.kr>
2026-08-14 12:57:29 +0900

    11

--- color_processing.cpp
+++ color_processing.cpp
@@ -1,4 +1,4 @@
-#include <iosteam>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
@@ -8,7 +8,22 @@ int main() {
 
     /*
         color processing: 전처리를 의미한다.
-        1. HSI로 변환한다. Hue, Saturation, Intensity
+        1. HSI로 변환한다.
+            Hue: 색상
+            Saturation: 순도, 선명도
+            Intensity: 밝기
+
+        Pseudo Coloring: 목적: 시각적으로 인지하기 편하게 하기 위함.
+        Color Balancing: 이미지의 색 편향을 보정해서 원래 색에 가깝게 만드는 작업
     */
+
+    Mat gray = imread("assets/xray.webp", 0);
+    Mat color;
+
+    applyColorMap(gray, color, COLORMAP_JET);
+    imshow("gray", gray);
+    imshow("image", color);
+
+    waitKey(0);
     return 0;
 }
